9a7bc5e5d7a1d6958e13cacfc8aaaf419f7fe5e0
lysu <sulifx@gmail.com>
2015-12-12 01:20:59 +0800

    return with point local variable.

--- asttest/ast.go
+++ asttest/ast.go
@@ -12,25 +12,77 @@ type testVisitor struct {
 	info types.Info
 }
 
-func (v *testVisitor) Visit(node ast.Node) (w ast.Visitor) {
+func (v *testVisitor) ifMethodReturnInterface(node ast.Node, funcDecl func(t *ast.FuncDecl)) {
 	switch t := node.(type) {
 	case *ast.FuncDecl:
-		if t.Type.Results != nil {
-			results := t.Type.Results.List
-			for _, result := range results {
-				tv := v.info.Types[result.Type]
-				fmt.Println(tv.Type, types.IsInterface(tv.Type))
-				if types.IsInterface(tv.Type) {
-					for _, stmt := range t.Body.List {
-						if rs, ok := stmt.(*ast.ReturnStmt); ok {
-							fmt.Println(rs.Results[0])
+		if t.Type.Results == nil {
+			return
+		}
+		results := t.Type.Results.List
+		for _, result := range results {
+			tv := v.info.Types[result.Type]
+			if !types.IsInterface(tv.Type) {
+				continue
+			}
+			funcDecl(t)
+		}
+	}
+}
+
+func (v *testVisitor) ifGenDecl(stmt ast.Stmt, genDecl func(genDecl *ast.GenDecl)) {
+	if decl, ok := stmt.(*ast.DeclStmt); ok {
+		if gDel, ok := decl.Decl.(*ast.GenDecl); ok {
+			if gDel.Tok != token.VAR {
+				return
+			}
+			genDecl(gDel)
+		}
+	}
+}
+
+var emptyStruct = struct{}{}
+
+func (v *testVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	v.ifMethodReturnInterface(node, func(t *ast.FuncDecl) {
+
+		mayNilVars := make(map[string]struct{})
+
+		for _, stmt := range t.Body.List {
+			v.ifGenDecl(stmt, func(genDecl *ast.GenDecl) {
+				for _, spec := range genDecl.Specs {
+					if varSpec, ok := spec.(*ast.ValueSpec); ok {
+						if _, ok := varSpec.Type.(*ast.StarExpr); !ok {
+							continue
+						}
+						for i, name := range varSpec.Names {
+							var value *ast.Expr
+							if len(varSpec.Values) > i {
+								value = &(varSpec.Values[i])
+							}
+							if value == nil || v.isNil(*value) {
+								mayNilVars[name.Name] = emptyStruct
+							}
+						}
+					}
+				}
+			})
+
+			if retStmt, ok := stmt.(*ast.ReturnStmt); ok {
+				for _, result := range retStmt.Results {
+					if ident, ok := result.(*ast.Ident); ok {
+						if _, ok := mayNilVars[ident.Name]; ok {
+							fmt.Printf("maybe return [%s] nil\n", ident.Name)
 						}
 					}
 				}
 			}
 		}
-	}
+	})
 	return v
+}
+
+func (v *testVisitor) isNil(e ast.Expr) bool {
+	return v.info.Types[e].Type == types.Typ[types.UntypedNil]
 }
 
 func main() {
@@ -45,11 +97,10 @@ func main() {
 		Uses:  make(map[*ast.Ident]types.Object),
 	}
 	var conf types.Config
-	pkg, err := conf.Check("fib", fset, []*ast.File{file}, &info)
+	_, err = conf.Check("fib", fset, []*ast.File{file}, &info)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(pkg.Name())
 	ast.Walk(&testVisitor{info}, file)
 	// printer.Fprint(os.Stdout, fset, file)
 }
@@ -65,12 +116,9 @@ func (t *TestErr) Error() string {
 	return "test err"
 }
 
-func test() *TestErr {
-	return nil
-}
-
 func b() error {
-	return test()
+    var err *TestErr = nil
+	return err
 }
 
 func main() {
