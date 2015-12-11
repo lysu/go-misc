9f40c5a63764a9c4a8ae770adddf645f86cb5bfe
lysu <sulifx@gmail.com>
2015-12-11 11:55:42 +0800

    check code test.

--- asttest/ast.go
+++ asttest/ast.go
@@ -5,25 +5,79 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"go/types"
 )
 
 type testVisitor struct {
+	info types.Info
 }
 
 func (v *testVisitor) Visit(node ast.Node) (w ast.Visitor) {
 	switch t := node.(type) {
 	case *ast.FuncDecl:
-		fmt.Printf("%s\n", t.Name.Name)
+		if t.Type.Results != nil {
+			results := t.Type.Results.List
+			for _, result := range results {
+				tv := v.info.Types[result.Type]
+				fmt.Println(tv.Type, types.IsInterface(tv.Type))
+				if types.IsInterface(tv.Type) {
+					for _, stmt := range t.Body.List {
+						if rs, ok := stmt.(*ast.ReturnStmt); ok {
+							fmt.Println(rs.Results[0])
+						}
+					}
+				}
+			}
+		}
 	}
 	return v
 }
 
 func main() {
 	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, "test_data/test.go", nil, 0)
+	file, err := parser.ParseFile(fset, "test.go", input, 0)
 	if err != nil {
 		panic(err)
 	}
-	ast.Walk(&testVisitor{}, file)
+	info := types.Info{
+		Types: make(map[ast.Expr]types.TypeAndValue),
+		Defs:  make(map[*ast.Ident]types.Object),
+		Uses:  make(map[*ast.Ident]types.Object),
+	}
+	var conf types.Config
+	pkg, err := conf.Check("fib", fset, []*ast.File{file}, &info)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pkg.Name())
+	ast.Walk(&testVisitor{info}, file)
 	// printer.Fprint(os.Stdout, fset, file)
 }
+
+const input = `
+package main
+
+type TestErr struct {
+	Msg string
+}
+
+func (t *TestErr) Error() string {
+	return "test err"
+}
+
+func test() *TestErr {
+	return nil
+}
+
+func b() error {
+	return test()
+}
+
+func main() {
+	if b() == nil {
+		panic("b == nil")
+	} else {
+		panic("b != nil")
+	}
+}
+`
