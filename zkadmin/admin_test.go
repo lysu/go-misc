446b5e38d7ed1697cd834cfa3c1fa244a494b7ce
lysu <sulifx@gmail.com>
2016-01-29 23:56:42 +0800

    use gopkg

--- zkadmin/admin_test.go
+++ zkadmin/admin_test.go
@@ -2,7 +2,7 @@ package zkadmin_test
 
 import (
 	"testing"
-	"github.com/wvanbergen/kazoo-go"
+	"gopkg.in/lysu/kazoo-go.v0"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,11 +11,12 @@ func TestCreateTopic(t *testing.T) {
 	config := kazoo.NewConfig()
 	kz, err := kazoo.NewKazoo([]string{"127.0.0.1:2181"}, config)
 	assert.NoError(t, err)
-	err = kz.CreateTopic("test_ddd1", 1, 1, make(map[string]interface{}))
+
+	err = kz.CreateTopic("test_ddd113", 1, 1, make(map[string]interface{}))
 	assert.NoError(t, err)
 
 
-	err = kz.DeleteTopic("test_ddd1")
+	err = kz.DeleteTopic("test_ddd113")
 	assert.NoError(t, err)
 
 }
